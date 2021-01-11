9458de267674c7a238160f53ae1422d46f1ce177
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-01-11 19:44:19 +0200

    feat: pomodoro app trial using svg

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+const circle = document.querySelector('circle');
+const radius = circle.r.baseVal.value;
+const circumference = radius * 2 * Math.PI;
 
-var circle = document.querySelector('circle');
-var radius = circle.r.baseVal.value;
-var circumference = radius * 2 * Math.PI;
+const displayTime = document.querySelector("time")
+console.log(circumference)
 
 circle.style.strokeDasharray = `${circumference} ${circumference}`;
 circle.style.strokeDashoffset = `${circumference}`;
@@ -9,15 +11,39 @@ circle.style.strokeDashoffset = `${circumference}`;
 function setProgress(percent) {
   const offset = circumference - percent / 60 * circumference;
   circle.style.strokeDashoffset = offset;
+  console.log(offset)
 }
 
-const input = document.querySelector('input');
-setProgress(input.value);
+//const input = document.querySelector('input');
+//setProgress(input.value);
 
-input.addEventListener('change', function(e) {
-  if (input.value < 61 && input.value > -1) {
-    setProgress(input.value);
-  }  
-})
+//input.addEventListener('change', function (e) {
+//  if (input.value < 61 && input.value > -1) {
+   // setProgress(input.value);
+ // }
+//})
 
+function timer(seconds) {
+ const temp = seconds
 
+ const progressChecker =  setInterval(function () {
+    
+    const mins = Math.floor(seconds / 60)
+    const displayMin = mins < 10 ? `0${mins}` : mins
+    const secs = seconds % 60
+    const displaySec = secs < 10 ? `0${secs}` : secs
+    seconds--
+    //console.log(seconds)
+   // console.log(temp)
+    //const cir = circumference - ((circumference * seconds) / temp)
+    //console.log(cir)
+    
+    if(seconds === 0) {
+      clearInterval(progressChecker)
+    }
+    displayTime.innerText = `${displayMin}:${displaySec}`
+    setProgress((temp - seconds))
+  }, 1000)
+}
+
+timer(10)
