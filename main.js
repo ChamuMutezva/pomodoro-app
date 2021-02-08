2fe52c29b58edf3ea459a451a2569810120501f7
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-02-08 05:49:28 +0200

    feat: modal settings page

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const circumference = radius * 2 * Math.PI;
 const TIME_LIMIT = 1500
 
 const displayTime = document.querySelector("time")
+
+const change__settings = document.querySelector(".menu__options--btn")
+const modal = document.querySelector(".modal__settings")
+console.log(change__settings)
 console.log(circumference)
 
 circle.style.strokeDasharray = `${circumference} ${circumference}`;
@@ -20,15 +24,15 @@ function setProgress(percent) {
 
 //input.addEventListener('change', function (e) {
 //  if (input.value < 61 && input.value > -1) {
-   // setProgress(input.value);
- // }
+// setProgress(input.value);
+// }
 //})
 
 function timer(seconds) {
-const temp = seconds
+  const temp = seconds
 
- const progressChecker =  setInterval(function () {
-    
+  const progressChecker = setInterval(function () {
+
     const mins = Math.floor(seconds / 60)
     const displayMin = mins < 10 ? `0${mins}` : mins
     const secs = seconds % 60
@@ -36,17 +40,22 @@ const temp = seconds
     seconds--
     displayTime.innerText = `${displayMin}:${displaySec}`
     //console.log(seconds)
-   // console.log(temp)
+    // console.log(temp)
     //const cir = circumference - ((circumference * seconds) / temp)
     //console.log(cir)
-    
-    if(seconds === 0) {
-      displayTime.innerHTML="00:00"
+
+    if (seconds === 0) {
+      displayTime.innerHTML = "00:00"
       clearInterval(progressChecker)
     }
-   
+
     setProgress((temp - seconds))
   }, 1000)
 }
 
 timer(TIME_LIMIT)
+
+/* modal setting */
+change__settings.addEventListener("click", () => {
+  modal.classList.toggle("modal__settings__hide")
+})
