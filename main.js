919f7752b2ff63754adff7e94e2b882af8860522
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-02-11 05:28:15 +0200

    control timers using event delegation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ring timer
 const circle = document.querySelector('circle');
 const radius = circle.r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
@@ -8,6 +9,9 @@ const displayTime = document.querySelector("time")
 const change__settings = document.querySelector(".menu__options--btn")
 const close__menu = document.querySelector(".close__menu")
 const modal = document.querySelector(".modal__settings")
+// timer controller setter
+const timer__controller = document.querySelector(".time__settings")
+console.log(timer__controller)
 //pomodoro time controllers
 const count__up = document.querySelector(".btn--up")
 const count__down = document.querySelector(".btn--down")
@@ -32,7 +36,7 @@ circle.style.strokeDashoffset = `${circumference}`;
 function setProgress(percent) {
   const offset = circumference - percent / TIME_LIMIT * circumference;
   circle.style.strokeDashoffset = offset;
-  console.log(offset)
+  //console.log(offset)
 }
 
 //const input = document.querySelector('input');
@@ -79,6 +83,7 @@ close__menu.addEventListener("click", () => {
   modal.classList.toggle("modal__settings__hide")
 })
 
+/*
 count__up.addEventListener("click", ()=> {
   if (count < 30) {
     count += 1
@@ -92,4 +97,21 @@ count__down.addEventListener("click", ()=> {
   } 
   input__control__time.value = count.toString()
   console.log(count)
+})*/
+timer__controller.addEventListener("click", (evt) => {
+  const target = evt.target
+  //get parent div of target
+  const parent__div = target.closest("div")
+  // get parent div of input element
+  const parent__input = parent__div.previousElementSibling
+  // get input element
+  const target__input = parent__input.firstElementChild
+  if (target.classList.contains("btn--up")) {
+    console.log("increase timer")
+    console.log(timer__controller.firstElementChild)
+  } else if(target.classList.contains("btn--down")) {
+    console.log("decrease the count down timer")
+    console.log(timer__controller.firstElementChild)
+  }
+ console.log(target__input)
 })
