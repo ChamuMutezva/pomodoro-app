49d41e5945aba9fcd735dab92863abd3cbfd0795
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-02-14 15:07:06 +0200

    feat: start and stop time

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-// ring timer
-const circle = document.querySelector('circle');
+// ring timer and time display
+const circle = document.querySelector('circle')
 const radius = circle.r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
 const TIME_LIMIT = 1500
-
 const displayTime = document.querySelector("time")
+const start__pause = document.querySelector(".start__pause")
+let paused = true
+console.log(start__pause)
 
-const change__settings = document.querySelector(".menu__options--btn")
-const close__menu = document.querySelector(".close__menu")
+const change__settings = document.querySelector(".menu__options--btn") //menu options selector
+const close__menu = document.querySelector(".close__menu") //close the settings menu
 const modal = document.querySelector(".modal__settings")
 const pomodoroBtn = document.querySelector(".pomodoro--btn") // to set focus on this button on load
 // timer controller setter
@@ -20,18 +22,12 @@ const count__down = document.querySelector(".btn--down")
 const input__control__time = document.querySelector(".time__control")
 const time__control__short = document.querySelector(".time__control__short")
 const time__control__long = document.querySelector(".time__control__long")
-
-
 let pomodoro__count = Number(input__control__time.value)
 let short__count = Number(time__control__short.value)
 let long__count = Number(time__control__long.value)
 console.log(pomodoro__count)
 
-const form = document.querySelector("form")
-
-window.onload = () => {
-  pomodoroBtn.focus()
-}
+const main__settings = document.querySelector(".main__ctrl__panel")
 
 console.log(change__settings)
 console.log(circumference)
@@ -78,7 +74,7 @@ change__settings.addEventListener("click", () => {
 })
 close__menu.addEventListener("click", () => {
   modal.classList.toggle("modal__settings__hide")
-  pomodoroBtn.focus()
+  //pomodoroBtn.focus()
 })
 
 timer__controller.addEventListener("click", (evt) => {
@@ -96,44 +92,44 @@ timer__controller.addEventListener("click", (evt) => {
     console.log("pomodoro timer control")
     if (target.classList.contains("btn--up")) {
       console.log("increase the pomodoro timer")
-      countUp(90, short__count, target__input, 5)     
+      countUp(90, short__count, target__input, 5)
     }
     else if (target.classList.contains("btn--down")) {
       console.log("decrease the pomodoro timer")
-      countDown(5, pomodoro__count, target__input, 90)    
+      countDown(5, pomodoro__count, target__input, 90)
     }
   } else if (target__input.classList.contains("time__control__short")) {
     console.log("short timer break controll")
     if (target.classList.contains("btn--up")) {
-      console.log("increase the short break timer")     
+      console.log("increase the short break timer")
       countUp(10, short__count, target__input, 5)
     }
     else if (target.classList.contains("btn--down")) {
-      console.log("decrease the short break timer")    
+      console.log("decrease the short break timer")
       countDown(5, short__count, target__input, 10)
     }
   } else if (target__input.classList.contains("time__control__long")) {
     console.log("long timer break controll")
     if (target.classList.contains("btn--up")) {
-      console.log("increase the long break timer")     
+      console.log("increase the long break timer")
       countUp(20, long__count, target__input, 11)
     }
     else if (target.classList.contains("btn--down")) {
-      console.log("decrease the long break timer")    
+      console.log("decrease the long break timer")
       countDown(11, long__count, target__input, 20)
     }
   }
-   console.log(target__input)
+  console.log(target__input)
 })
 
 const countUp = (maxvalue, counter, targetInput, checkmin) => {
 
-  if(Number(targetInput.value < checkmin)) {
+  if (Number(targetInput.value < checkmin)) {
     //alert(`Minimum value should not be below ${checkmin}`)
     targetInput.value = checkmin
-  }  
+  }
 
-  if(Number(targetInput.value > maxvalue)) {
+  if (Number(targetInput.value > maxvalue)) {
     //alert(`Maximum value should not exceed ${maxvalue}`)
     targetInput.value = maxvalue
   }
@@ -143,15 +139,15 @@ const countUp = (maxvalue, counter, targetInput, checkmin) => {
     targetInput.value = counter
     console.log(counter)
     console.log(maxvalue)
-    console.log(targetInput.value)   
-  }  
+    console.log(targetInput.value)
+  }
 }
 
 const countDown = (minvalue, counter, targetInput, checkmax) => {
-  if(Number(targetInput.value < minvalue)) {
+  if (Number(targetInput.value < minvalue)) {
     //alert(`Minimum value should not be below ${minvalue}`)
     targetInput.value = minvalue
-  }  
+  }
 
   counter = Number(targetInput.value)
   if (counter > minvalue) {
@@ -159,7 +155,7 @@ const countDown = (minvalue, counter, targetInput, checkmax) => {
     targetInput.value = counter
     console.log(counter)
     console.log(minvalue)
-    console.log(targetInput.value)   
+    console.log(targetInput.value)
   }
 
   if (Number(targetInput.value > checkmax)) {
@@ -168,6 +164,13 @@ const countDown = (minvalue, counter, targetInput, checkmax) => {
   }
 }
 
-form.addEventListener("submit", (evt)=> {
+main__settings.addEventListener("submit", (evt) => {
   evt.preventDefault()
+})
+
+start__pause.addEventListener("click", () => {
+  console.log("start and pause button")
+  paused = !paused
+  console.log(paused) 
+  paused ? start__pause.innerHTML = "Start" : start__pause.innerHTML = "Pause"
 })
